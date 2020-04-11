d340ecf224770032ba13e48d98508864113ad3a8
Phoenix181 <aadhishrm11@gmail.com>
2020-04-11 23:41:16 +0530

    Added my configs

---
+++ emacs/.emacs.d/init.el
@@ -0,0 +1,17 @@
+(org-babel-load-file (concat user-emacs-directory "config.org"))
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(elcord-use-major-mode-as-main-icon t)
+ '(package-selected-packages
+   (quote
+    (treemacs-icons-dired treemacs-magit treemacs-projectile which-key vue-mode use-package tide rjsx-mode react-snippets magit lsp-ui emmet-mode elcord doom-modeline dap-mode company-lsp cmake-mode ccls add-node-modules-path))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
