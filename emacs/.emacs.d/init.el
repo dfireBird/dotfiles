08af5d27cb155d1d2947dd78f2fe1ec03a527a05
Phoenix181 <aadhishrm11@gmail.com>
2020-04-13 10:34:24 +0530

    Vterm, multiplecursor, table of contents and org mode

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -1,3 +1,5 @@
+(setq vc-follow-symlinks t)
+
 (org-babel-load-file (concat user-emacs-directory "config.org"))
 
 (custom-set-variables
@@ -6,9 +8,10 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(elcord-use-major-mode-as-main-icon t)
+ '(org-agenda-files nil)
  '(package-selected-packages
    (quote
-    (treemacs-icons-dired treemacs-magit treemacs-projectile which-key vue-mode use-package tide rjsx-mode react-snippets magit lsp-ui emmet-mode elcord doom-modeline dap-mode company-lsp cmake-mode ccls add-node-modules-path))))
+    (wakatime-mode treemacs-icons-dired treemacs-magit treemacs-projectile which-key vue-mode use-package tide rjsx-mode react-snippets magit lsp-ui emmet-mode elcord doom-modeline dap-mode company-lsp cmake-mode ccls add-node-modules-path))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
--- zsh/.zshrc
+++ zsh/.zshrc
@@ -137,3 +137,17 @@ extract ()
     echo "'$1' is not a valid file"
   fi
 }
+
+# Emacs-Vterm config
+function vterm_printf() {
+    if [ -n "$TMUX" ]; then
+        # Tell tmux to pass the escape sequences through
+        # (Source: http://permalink.gmane.org/gmane.comp.terminal-emulators.tmux.user/1324)
+        printf "\ePtmux;\e\e]%s\007\e\\" "$1"
+    elif [ "${TERM%%-*}" = "screen" ]; then
+        # GNU screen (screen, screen-256color, screen-256color-bce)
+        printf "\eP\e]%s\007\e\\" "$1"
+    else
+        printf "\e]%s\e\\" "$1"
+    fi
+}
